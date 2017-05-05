b83263df0c53fd57dc8c0398684f5ab0ba3fd012
dani <dani.zaghian@gmail.com>
2017-05-04 21:16:44 -0700

    added saving recipe via form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,16 @@ class RecipesController < ApplicationController
     @recipe = Recipe.first
     render "single_recipe.html.erb"
   end
+
+  def new_recipe_form
+    render "new_recipe_form.html.erb"
+  end
+
+  def new_recipe_result
+    @new_recipe = Recipe.create(
+      title: params[:recipe_title], 
+      chef: params[:recipe_chef]
+      )
+    render "new_recipe_result.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/single_recipe" => "recipes#single_recipe"
+
+  get "/new_recipe_form" => "recipes#new_recipe_form"
+  post "/new_recipe_result" => "recipes#new_recipe_result"
+  
 end
